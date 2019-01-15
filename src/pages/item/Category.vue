ff3867c49931643b8cf0154f407f5f628de0fa33
xiangxiao1995 <46650244+xiangxiao1995@users.noreply.github.com>
2019-01-15 22:37:36 +0800

    品牌查询前台

--- src/pages/item/Category.vue
+++ src/pages/item/Category.vue
@@ -2,7 +2,6 @@
   <v-card>
       <v-flex xs12 sm10>
         <v-tree url="/item/category/list"
-                :treeData="treeData"
                 :isEdit="isEdit"
                 @handleAdd="handleAdd"
                 @handleEdit="handleEdit"
@@ -14,12 +13,10 @@
 </template>
 
 <script>
-  import {treeData} from '../../mockDB'
   export default {
     name: "category",
     data() {
       return {
-        treeData: treeData,
         isEdit:true
       }
     },
